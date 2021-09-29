02aa1460b370a7b38799bda9a7be6969a25c2c70
dmitrygalayev <Neoclassic16@gmail.com>
2021-09-29 13:58:31 +0300

    Добавлены две функции, подключен main.js

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function getRndInteger(min, max) {
+  if (min < 0 || max < 0 || min >= max) {
+    const message = 'Некорректные входные данные: min < 0, max < 0 или min >= max';
+    console.log(message);
+    return 0;
+  }
+  const randomNumber = Math.floor(Math.random() * (max - min + 1) ) + min;
+  return randomNumber;
+}
+getRndInteger(); // Функция №1
+
+
+function commentLength (line, maximumLength) {
+  return !((line.length >= maximumLength));
+}
+commentLength(); // Функция №2
+

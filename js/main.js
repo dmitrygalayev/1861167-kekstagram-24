17c517aede0e6beff8f1629bd3da544af54e19da
dmitrygalayev <Neoclassic16@gmail.com>
2021-09-30 13:33:13 +0300

    Исправлены замечания

--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
-function getRndInteger(min, max) {
+const getRndInteger = function (min, max) {
   if (min < 0 || max < 0 || min >= max) {
     const message = 'Некорректные входные данные: min < 0, max < 0 или min >= max';
     // eslint-disable-next-line no-console
     console.log(message);
     return 0;
   }
-  const randomNumber = Math.floor(Math.random() * (max - min + 1) ) + min;
-  return randomNumber;
-}
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+};
 getRndInteger(); // Функция №1
 
-
 function commentLength (line, maximumLength) {
-  return !((line.length >= maximumLength));
+  return !!((line <= maximumLength && maximumLength < 141));
 }
 commentLength(); // Функция №2
-

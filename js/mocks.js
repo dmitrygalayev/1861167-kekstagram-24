3a360909383242efebeafc6c5e714a6c53b9108a
dmitrygalayev <Neoclassic16@gmail.com>
2021-10-27 12:35:05 +0300

    Продолжение работы

--- js/mocks.js
+++ js/mocks.js
@@ -1,17 +1,20 @@
 import {getStringFromArray, getRndIntArray, getRndInteger, getRndNumberOfItemsFromArray} from './utils.js';
 import {COMMENT_MESSAGES, COMMENT_MESSAGES_MAX_LENGTH, COMMENT_NAMES, AVATAR_INDEX, COMMENT_AMOUNT_RANGE, DESCRIPTIONS, LIKES_INDEX, PHOTOS_INDEXS} from './constants.js';
+
 const getComment = (id) => ({
   id: id,
   avatar: `img/avatar-${getRndInteger(AVATAR_INDEX.min, AVATAR_INDEX.max)}.svg`,
   message: getStringFromArray(getRndNumberOfItemsFromArray(COMMENT_MESSAGES, COMMENT_MESSAGES_MAX_LENGTH)),
   name: getRndNumberOfItemsFromArray(COMMENT_NAMES),
 });
+
 // Генерирует от рандомное количество (в заданном диапазоне) объектов с данными о комментариях
 const getComments = (
   id,
   length = getRndInteger(COMMENT_AMOUNT_RANGE.min, COMMENT_AMOUNT_RANGE.max),
 // eslint-disable-next-line id-length
 ) => Array.from({length}, (int, i) => getComment(id + 10000 + i + 1));
+
 // Генерирует объект с информацией о фотографии
 const getPhoto = (id = 1) => ({
   id,
@@ -20,6 +23,7 @@ const getPhoto = (id = 1) => ({
   likes: getRndInteger(LIKES_INDEX.min, LIKES_INDEX.max),
   comments: getComments(id),
 });
+
 // Генерирует массив заданной длины из объектов, содержащих информацию о фотографии
 const getPhotos = () => getRndIntArray(PHOTOS_INDEXS.min, PHOTOS_INDEXS.max, PHOTOS_INDEXS.max).map(
   (id) => getPhoto(id),
--- js/previews.js
+++ js/previews.js
@@ -2,32 +2,73 @@ import {mockData} from './mocks.js';
 
 const previewTemplate = document.querySelector('#picture').content;
 const pictureContainer = document.querySelector('.pictures');
-// const bigPicture = document.querySelector('.big-picture');
-// const bigPictureImage = bigPicture.querySelector('.big-picture__img');
-const fragment = document.createDocumentFragment();
+const bigPicture = document.querySelector('.big-picture');
+const bigPictureImage = bigPicture.querySelector('.big-picture__img img');
 
-// const onPreviewClick = (element) => {
-//   element.addEventListener('click', (evt) => {
-//     evt.preventDefault();
-//     bigPicture.classList.remove('hidden');
-//     bigPictureImage.src = evt.target.querySelector('img').src;
-//   });
-// };
+const commentList = document.querySelector('.social__comments');
+const commentListItem = commentList.querySelector('.social__comments li');
 
-for (let i = 0; i < mockData.length; i++) {
-  const template = previewTemplate.cloneNode(true);
-  const image = template.querySelector('img');
-  const likes = template.querySelector('.picture__likes');
-  const comments = template.querySelector('.picture__comments');
+const closeButton = bigPicture.querySelector('#picture-cancel');
 
-  comments.textContent = mockData[i].comments.length;
-  likes.textContent = mockData[i].likes;
-  image.src = mockData[i].url;
-  // onPreviewClick(template.querySelector('a'));
-  fragment.appendChild(template);
+const createComments = (parentNode, data) => {
+  const fragment = document.createDocumentFragment();
 
-}
+  for (let i = 0; i < data.length; i++) {
+    const template = commentListItem.cloneNode(true);
 
-pictureContainer.appendChild(fragment);
-export {previewTemplate};
+    // const avatar = template.querySelector('img');
+    // const image = template.querySelector('img');
+    // const likes = template.querySelector('.picture__likes');
+    // const comments = template.querySelector('.picture__comments');
 
+    // comments.textContent = data[i].comments.length;
+    // likes.textContent = data[i].likes;
+    // image.src = data[i].url;
+    // onPreviewClick(template.querySelector('a'));
+    fragment.appendChild(template);
+  }
+  parentNode.textContent = '';
+  parentNode.appendChild(fragment);
+};
+createComments(commentList, mockData);
+
+const createPhotos = (data) => {
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < data.length; i++) {
+    const template = previewTemplate.cloneNode(true);
+    const image = template.querySelector('img');
+    const likes = template.querySelector('.picture__likes');
+    const comments = template.querySelector('.picture__comments');
+
+    comments.textContent = data[i].comments.length;
+    likes.textContent = data[i].likes;
+    image.src = data[i].url;
+    fragment.appendChild(template);
+  }
+  pictureContainer.appendChild(fragment);
+};
+
+createPhotos(mockData);
+
+pictureContainer.addEventListener('click', (evt) => {
+  if (!evt.target.classList.contains('picture__img')) {
+    return;
+  }
+  const src = evt.target.src;
+  evt.preventDefault();
+  bigPicture.classList.remove('hidden');
+  bigPictureImage.src = src;
+  const comments = mockData.find((item) => src.includes(item.url)).comments;
+  createComments(commentList, comments);
+});
+
+closeButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  bigPicture.classList.add('hidden');
+});
+
+document.addEventListener('keydown', (evt) => {
+  if (evt.keyKode === 27) {
+    bigPicture.classList.add('hidden');
+  }
+});

7bd1c20aa15a4b4928b555fde44223b2f324a909
dmitrygalayev <Neoclassic16@gmail.com>
2021-11-04 23:21:24 +0300

    Начало работы

--- js/bigPicture.js
+++ js/bigPicture.js
@@ -1,3 +1,4 @@
+import { STEP } from './constants.js';
 import { mockData } from './mocks.js';
 
 const body = document.querySelector('body');
@@ -8,12 +9,21 @@ const pictureContainer = document.querySelector('.pictures');
 const commentList = bigPicture.querySelector('.social__comments');
 const overlay = document.querySelector('.overlay');
 const commentListItem = bigPicture.querySelector('.social__comment');
+const commentsLoadButton = bigPicture.querySelector('.social__comments-loader');
+const totalCommentsCount = bigPicture.querySelector('.comments-count');
+const visibleCommentsCount = bigPicture.querySelector('.visible-comments-count');
 
 
-const createComments = (parentNode, data) => {
+let commentsArray = [];
+let currentCommentsCount = STEP;
+
+const createComments = (parentNode, data, startIndex = 0, currentStep = STEP, clearNode = true) => {
   const fragment = document.createDocumentFragment();
-  parentNode.textContent = '';
-  for (let i = 0; i < data.length; i++) {
+  const commentsCount = data.length > currentStep ? currentStep : data.length;
+
+  if (clearNode) {parentNode.textContent = '';}
+
+  for (let i = startIndex; i < commentsCount; i++) {
     const commentTemplate = commentListItem.cloneNode(true);
     const commentContent = commentTemplate.querySelector('img');
     const commentText = commentTemplate.querySelector('p');
@@ -45,14 +55,40 @@ const onPictureContainerClick = (evt) => {
   bigPictureOpen();
   const src = evt.target.src;
   bigPictureImage.src = src;
-  bigPicture.querySelector('.social__comment-count').classList.add('hidden');
-  bigPicture.querySelector('.comments-loader').classList.add('hidden');
 
   const {comments, description} = mockData.find((item) => src.includes(item.url));
+  commentsArray = [...comments];
+  currentCommentsCount = STEP;
+
+  if (comments.length === 0) {
+    bigPicture.querySelector('.social__comment-count').classList.add('hidden');
+    bigPicture.querySelector('.comments-loader').classList.add('hidden');
+  } else if (comments.length < 5 && comments.length > 0) {
+    bigPicture.querySelector('.comments-loader').classList.add('hidden');
+    bigPicture.querySelector('.social__comment-count').classList.remove('hidden');
+    visibleCommentsCount.textContent = comments.length < STEP ? comments.length : STEP;
+    totalCommentsCount.textContent = comments.length;
+  } else {
+    totalCommentsCount.textContent = comments.length;
+    visibleCommentsCount.textContent = comments.length < STEP ? comments.length : STEP;
+    bigPicture.querySelector('.comments-loader').classList.remove('hidden');
+    bigPicture.querySelector('.social__comment-count').classList.remove('hidden');
+  }
 
   bigPicture.querySelector('.social__caption').textContent = description;
   createComments(commentList, comments);
 };
+
+commentsLoadButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  createComments(commentList, commentsArray, currentCommentsCount, currentCommentsCount + STEP, false);
+  currentCommentsCount = currentCommentsCount + STEP;
+  visibleCommentsCount.textContent =
+    currentCommentsCount > commentsArray.length ? commentsArray.length : currentCommentsCount;
+  if (currentCommentsCount >= commentsArray.length) {
+    bigPicture.querySelector('.comments-loader').classList.add('hidden');// toDo вынести в константу
+  }
+});
 
 pictureContainer.addEventListener('click', onPictureContainerClick);
 
@@ -67,6 +103,8 @@ document.addEventListener('keydown', (evt) => {
   }
 });
 
-overlay.addEventListener('click', () => {
-  bigPictureClose();
+overlay.addEventListener('click', (evt) => {
+  if (evt.target === overlay) {
+    bigPictureClose();
+  }
 });
--- js/forms.js
+++ js/forms.js
@@ -1,27 +1,62 @@
-// import { body } from './bigPicture.js';
+import { MAX_COMMENT_LENGTH } from './constants.js';
+import { checkStringLength } from './utils/check-string-length.js';
 
-const body = document.querySelector('body');
-const imageUploadButton = body.querySelector('#upload-file');
-const uploadOverlay = body.querySelector('.img-upload__overlay');
-const uploadCancelButton = body.querySelector('#upload-cancel');
-const newImageDescription = body.querySelector('.text__description');
+const form = document.querySelector('#upload-select-image');
+const imageUploadButton = form.querySelector('#upload-file');
+const uploadOverlay = form.querySelector('.img-upload__overlay');
+const uploadCancelButton = form.querySelector('#upload-cancel');
+const newImageDescription = form.querySelector('.text__description');
+const hashtags = form.querySelector('.text__hashtags');
 
 const editImageFormOpen = () => {
-  body.classList.add('modal-open');
+  document.body.classList.add('modal-open');
   uploadOverlay.classList.remove('hidden');
 };
 
 const editImageFormClose = () => {
-  body.classList.remove('modal-open');
+  document.body.classList.remove('modal-open');
   uploadOverlay.classList.add('hidden');
+  imageUploadButton.value = '';
 };
 
-newImageDescription.addEventListener('invalid', () => {
-  if (newImageDescription.validity.tooLong) {
+newImageDescription.addEventListener('input', (evt) => {
+  if (!checkStringLength(evt.target.value, MAX_COMMENT_LENGTH)) {
     newImageDescription.setCustomValidity('Описание не должно превышать 140 символов');
   } else {
     newImageDescription.setCustomValidity('');
   }
+  newImageDescription.reportValidity();
+});
+
+hashtags.addEventListener('input', (evt) => {
+  evt.preventDefault();
+  const normalizedValue = hashtags.value.toLowerCase();
+  const values = normalizedValue.split(/\s/).filter(Boolean);
+
+  if (!hashtags.value) {
+    hashtags.setCustomValidity('');
+  }
+  if (values.length > 5) {
+    hashtags.setCustomValidity('Нельзя указать более 5ти хэштегов');
+  } else if (!(/^[а-яА-Я-ёЁa-zA-Z0-9#]+$/).test(normalizedValue)) {
+    hashtags.setCustomValidity('Хэштег не может содержать пробелы, спецсимволы и символы пунктуации');
+  } else {
+    values.for ((value, index) => {
+      if (value.charAt() !== '#') {
+        hashtags.setCustomValidity('Хэштеги должны начинаться с символа #');
+      } else if (value.length === 1 && value.length > 20) {
+        hashtags.setCustomValidity ('Хэштег не может состоять из одной решетки или быть  длиннее 20-ти знаков');
+      } else if (values.includes(value, index + 1)) {
+        hashtags.setCustomValidity('Один и тот же хэш-тег не может быть использован дважды');
+      } else if (value.substring(1).includes('#')) {
+        hashtags.setCustomValidity ('Хэштег не может содержать пробелы, спецсимволы и символы пунктуации');
+      } else {
+        hashtags.setCustomValidity('');
+      }
+
+    });
+  }
+  hashtags.reportValidity();
 });
 
 imageUploadButton.addEventListener('change', (evt) => {
@@ -36,6 +71,10 @@ uploadCancelButton.addEventListener('click', (evt) => {
 });
 
 document.addEventListener('keydown', (evt) => {
+  if (evt.target === hashtags || evt.target === newImageDescription) {
+    return;
+  }
+
   if (evt.keyCode === 27) {
     editImageFormClose();
   }

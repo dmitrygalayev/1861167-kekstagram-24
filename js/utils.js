ed0812cf38bbf26fb902bb3e950fbd2c16ea5257
dmitrygalayev <Neoclassic16@gmail.com>
2021-11-17 11:45:50 +0300

    Подчищен неиспользуемый код

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,3 @@
-import {} from './constants.js';
-// // const commentLength = (line, maximumLength) => line.length <= maximumLength;
-// Генерирует рандомное число в заданном диапазоне
 const getRndInteger = (min, max) => {
   if (min < 0 || max < 0 || min >= max) {
     const message = 'Некорректные входные данные: min < 0, max < 0 или min >= max';
@@ -10,42 +7,8 @@ const getRndInteger = (min, max) => {
   }
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 };
-// Cоединяет элементы массива с переносом строки
-const getStringFromArray = (array) => array.join('\n');
-// Создает массив целых чисел заданной длины в заданном диапазоне
-const getRndIntArray = (min, max, length) => {
-  if(!length) {
-    return;
-  }
-  const array = [];
-  if (max - min + 1 < length) {
-    // eslint-disable-next-line no-console
-    console.log(max - min < length);
-    return [];
-  }
 
-  for (let index = 0; index < Infinity; index++) {
-    const rndInt = getRndInteger(min, max);
-
-    if (!array.includes(rndInt)) {
-      array.push(rndInt);
-    }
-
-    if (array.length === length) {
-      break;
-    }
-  }
-  return array;
-};
-// Выбирает рандомное количество элементов из массива
-const getRndNumberOfItemsFromArray = (array, maxLength) => {
-  const arrayLength = getRndInteger(1, maxLength) || 1;
-  const rndArray = getRndIntArray(0, array.length - 1, arrayLength);
-  // eslint-disable-next-line id-length
-  return Array.from({length: arrayLength}, (item, i) => array[rndArray[i]]);
-};
-
-const getRandomArrayFromData = (data, count) => {
+export const getRandomArrayFromData = (data, count) => {
   const array = [];
   while (array.length < count) {
     const randomIndex = getRndInteger(0, data.length - 1);
@@ -55,4 +18,3 @@ const getRandomArrayFromData = (data, count) => {
   }
   return array;
 };
-export {getRndInteger, getRndIntArray, getRndNumberOfItemsFromArray, getStringFromArray, getRandomArrayFromData};
--- js/utils/get-random-positive-integer.js
+++ js/utils/get-random-positive-integer.js
@@ -1,7 +1,7 @@
 // Функция взята из интернета и доработана
 // Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
 
-/*function getRndInteger (a, b) {
+function getRndInteger (a, b) {
   // Чтобы не заставлять пользователя нашей функции помнить порядок аргументов,
   // реализуем поддержку передачи минимального и максимального значения в любом порядке,
   // а какое из них большее и меньшее вычислим с помощью Math.min и Math.max.
@@ -26,4 +26,5 @@
   // потому что Math.random() генерирует только дробные числа и ноль.
   return Math.floor(result);
 }
-getRndInteger ()*/
+getRndInteger ();
+

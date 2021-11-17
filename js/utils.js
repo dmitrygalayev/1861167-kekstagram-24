2cd0d3d02f10e30d2812fdfc447f9d843b7561d0
dmitrygalayev <Neoclassic16@gmail.com>
2021-11-17 19:03:46 +0300

    Исправлены ошибки по пункту Б11

--- js/utils.js
+++ js/utils.js
@@ -1,9 +1,7 @@
-const getRndInteger = (min, max) => {
+export const getRndInteger = (min, max) => {
   if (min < 0 || max < 0 || min >= max) {
     const message = 'Некорректные входные данные: min < 0, max < 0 или min >= max';
-    // eslint-disable-next-line no-console
-    console.log(message);
-    return 0;
+    throw new Error (message);
   }
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 };
